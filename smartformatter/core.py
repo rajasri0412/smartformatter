77530b11a919a7d9db5aceb6606c38c9a3498d0c
rajasri0412 <rajasrideshinni@gmail.com>
2025-07-21 16:48:07 +0530

    removed function format_Phone

--- smartformatter/core.py
+++ smartformatter/core.py
@@ -4,10 +4,6 @@ import inflect
 def format_name(name):
     return ' '.join([w.capitalize() for w in name.strip().split()])
 
-def format_phone(phone):
-    digits = re.sub(r'\D', '', phone)
-    return f"+{digits[:2]} {digits[2:7]} {digits[7:]}"
-
 def format_currency(amount, symbol='$'):
     return f"{symbol}{amount:,.2f}"
 
